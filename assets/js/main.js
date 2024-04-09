0bfab71e529cefa8f00419aeff7ec39df3a0eb0f
Dev-Abdalla-Magdy <eng.abdalla.magdy@outlook.com>
2024-04-09 04:19:41 +0200

    Finish 50% from Landing Section & some Editing in Header Section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,24 +2,41 @@ document.addEventListener("DOMContentLoaded", function () {
   const linksMenu = document.querySelector(".links");
   const burgerMenu = document.querySelector("#menu");
   const allBarsMenu = document.querySelectorAll(".bar");
-  const dropListBtns = document.querySelectorAll(".drop_list");
-  const dropList = document.querySelectorAll(".b_links");
   const mainLinks = document.querySelectorAll(".main_link");
+  const listBtns = document.querySelectorAll(".list_btn");
 
   burgerMenu.addEventListener("click", function () {
     allBarsMenu.forEach((item) => item.classList.toggle("active"));
     linksMenu.classList.toggle("active");
   });
 
-  dropListBtns.forEach((item) =>
-    item.addEventListener("click", (e) => {
-      console.log(e);
+  mainLinks.forEach((link) =>
+    link.addEventListener("mouseenter", (e) => {
+      // e.preventDefault();
+      const targetId = link.dataset.target;
+      const targetDropList = document.getElementById(targetId);
+      targetDropList.classList.remove("inactive");
     })
   );
 
   mainLinks.forEach((link) =>
-    link.addEventListener("mouseover", (e) => {
-      console.log(e.target, "hi");
+    link.addEventListener("mouseleave", (e) => {
+      // e.preventDefault();
+      const targetId = link.dataset.target;
+      const targetDropList = document.getElementById(targetId);
+      targetDropList.classList.add("inactive");
+    })
+  );
+
+  listBtns.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      const targetId = btn.dataset.target;
+      const targetDropList = document.getElementById(targetId);
+      console.log(targetDropList.id);
+      if (targetDropList) {
+        targetDropList.classList.toggle("inactive");
+      }
     })
   );
 });
