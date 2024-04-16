12a50c8497393171b883d49150d273031dd9dc63
Dev-Abdalla-Magdy <eng.abdalla.magdy@outlook.com>
2024-04-16 22:47:55 +0200

    Almost Done With Header Section & Fix Some Issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,68 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const linksMenu = document.querySelector(".links");
-  const burgerMenu = document.querySelector("#menu");
-  const allBarsMenu = document.querySelectorAll(".bar");
-  const mainLinks = document.querySelectorAll(".main_link");
-  const listBtns = document.querySelectorAll(".list_btn");
+const linksMenu = document.querySelector(".links");
+const burgerMenu = document.querySelector("#menu");
+const allBarsMenu = document.querySelectorAll(".bar");
 
-  burgerMenu.addEventListener("click", function () {
-    allBarsMenu.forEach((item) => item.classList.toggle("active"));
-    linksMenu.classList.toggle("active");
-  });
+burgerMenu.addEventListener("click", function () {
+  allBarsMenu.forEach((item) => item.classList.toggle("active"));
+  linksMenu.classList.toggle("active");
+});
 
-  mainLinks.forEach((link) =>
-    link.addEventListener("mouseenter", (e) => {
-      // e.preventDefault();
-      const targetId = link.dataset.target;
-      const targetDropList = document.getElementById(targetId);
-      targetDropList.classList.remove("inactive");
-    })
-  );
+/* ===== 🔽 Header Drop List Menu 🔽 =====*/
+const dropListBtn1 = document.getElementById("drop_list_btn_1");
+const dropList1 = document.getElementById("drop_list_1");
+const dropListBtn2 = document.getElementById("drop_list_btn_2");
+const dropList2 = document.getElementById("drop_list_2");
 
-  mainLinks.forEach((link) =>
-    link.addEventListener("mouseleave", (e) => {
-      // e.preventDefault();
-      const targetId = link.dataset.target;
-      const targetDropList = document.getElementById(targetId);
-      targetDropList.classList.add("inactive");
-    })
-  );
+dropListBtn1.addEventListener("click", () => {
+  dropList1.classList.toggle("inactive");
+  if (!dropList2.classList.contains("inactive")) {
+    dropList2.classList.add("inactive");
+  }
+});
 
-  listBtns.forEach((btn) =>
-    btn.addEventListener("click", (e) => {
-      e.preventDefault();
-      const targetId = btn.dataset.target;
-      const targetDropList = document.getElementById(targetId);
-      console.log(targetDropList.id);
-      if (targetDropList) {
-        targetDropList.classList.toggle("inactive");
-      }
-    })
-  );
+dropListBtn2.addEventListener("click", () => {
+  dropList2.classList.toggle("inactive");
+  if (!dropListBtn1.classList.contains("inactive")) {
+    dropList1.classList.add("inactive");
+  }
+});
+
+document.addEventListener("click", function (event) {
+  const isClicked = event.target.closest("li");
+
+  if (!isClicked) {
+    dropList1.classList.add("inactive");
+    dropList2.classList.add("inactive");
+  }
+});
+/* ===== 🔼 Header Drop List Menu 🔼 =====*/
+const themeBtn = document.getElementById("theme_btn");
+const darkBtn = document.getElementById("dark_btn");
+const lightBtn = document.getElementById("light_btn");
+const bodyEl = document.body;
+
+function applyTheme(isDarkTheme) {
+  if (isDarkTheme) {
+    bodyEl.style.backgroundColor = "#0f0f0f";
+    bodyEl.style.color = "#fbfbfb";
+    darkBtn.classList.add("inactive");
+    lightBtn.classList.remove("inactive");
+  } else {
+    bodyEl.style.backgroundColor = "#fbfbfb";
+    bodyEl.style.color = "#0f0f0f";
+    lightBtn.classList.add("inactive");
+    darkBtn.classList.remove("inactive");
+  }
+}
+
+let isDarkTheme = localStorage.getItem("darkTheme") === "true";
+
+applyTheme(isDarkTheme);
+
+themeBtn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const newIsDarkTheme = !isDarkTheme;
+  applyTheme(newIsDarkTheme);
+  localStorage.setItem("darkTheme", newIsDarkTheme);
+  isDarkTheme = newIsDarkTheme;
 });
